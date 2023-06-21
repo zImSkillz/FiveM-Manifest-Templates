3c1cab4633ecaa49bc66aa1145fd677f40263c98
zImSkillz <83404249+zImSkillz@users.noreply.github.com>
2023-06-21 14:14:57 +0200

    Create fxmanifest.lua

---
+++ vehicle/fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+game 'gta5'
+
+files {
+    '*.meta'
+}
+
+data_file 'HANDLING_FILE'            '*.meta'
+data_file 'VEHICLE_LAYOUTS_FILE'    '*.meta'
+data_file 'VEHICLE_METADATA_FILE'    '*.meta'
+data_file 'CARCOLS_FILE'            '*.meta'
+data_file 'VEHICLE_VARIATION_FILE'    '*.meta'
+data_file 'CONTENT_UNLOCKING_META_FILE' '*.meta'
+data_file 'PTFXASSETINFO_FILE' '*.meta'
+
+client_script {
+    '*.lua'
+}
