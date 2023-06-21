906422e9bfebb319b733867274a2022c0c842ef0
zImSkillz <83404249+zImSkillz@users.noreply.github.com>
2023-06-21 14:12:23 +0200

    Create fxmanifest.lua

---
+++ vehiclePack/fxmanifest.lua
@@ -0,0 +1,40 @@
+fx_version 'cerulean'
+game 'gta5'
+
+files {
+    '*.meta',
+    'metas/**/*.meta',
+    'metas/**/**/*.meta'
+}
+
+data_file 'HANDLING_FILE' '*.meta'
+
+data_file 'CARCOLS_FILE' 'metas/**/*.meta'
+data_file 'VEHICLE_METADATA_FILE' 'metas/**/*.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'metas/**/*.meta'
+
+data_file 'HANDLING_FILE'            'metas/**/*.meta'
+data_file 'VEHICLE_LAYOUTS_FILE'    'metas/**/*.meta'
+data_file 'VEHICLE_METADATA_FILE'    'metas/**/*.meta'
+data_file 'CARCOLS_FILE'            'metas/**/*.meta'
+data_file 'VEHICLE_VARIATION_FILE'    'metas/**/*.meta'
+data_file 'CONTENT_UNLOCKING_META_FILE' 'metas/**/*.meta'
+data_file 'PTFXASSETINFO_FILE' 'metas/**/*.meta'
+
+data_file 'CARCOLS_FILE' 'metas/**/**/*.meta'
+data_file 'VEHICLE_METADATA_FILE' 'metas/**/**/*.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'metas/**/**/*.meta'
+
+data_file 'HANDLING_FILE'            'metas/**/**/*.meta'
+data_file 'VEHICLE_LAYOUTS_FILE'    'metas/**/**/*.meta'
+data_file 'VEHICLE_METADATA_FILE'    'metas/**/**/*.meta'
+data_file 'CARCOLS_FILE'            'metas/**/**/*.meta'
+data_file 'VEHICLE_VARIATION_FILE'    'metas/**/**/*.meta'
+data_file 'CONTENT_UNLOCKING_META_FILE' 'metas/**/**/*.meta'
+data_file 'PTFXASSETINFO_FILE' 'metas/**/**/*.meta'
+
+client_script {
+    '*.lua',
+    'metas/**/*.lua',
+    'metas/**/**/*.lua'
+}
